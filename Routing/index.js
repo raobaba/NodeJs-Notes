de54644d1fc9e19bb2e2c1675b9315ddcafd4caa
raobaba <raorajan9576@gmail.com>
2023-04-26 13:17:55 +0530

    handle route form html

--- Routing/index.js
+++ Routing/index.js
@@ -1,14 +1,18 @@
 const http = require('http');
+const fs = require("fs");
+const html = fs.readFileSync('./index.html','utf-8')
 const server = http.createServer((req,res)=>{
      let path = req.url;
      if(path==='/'||path==='/home'){
-        res.end("You are in Home Page");
-     }else if(path==='/about'){
-        res.end("You are in About Page");
-     }else if(path==='/contact'){
-        res.end("You are in Contact Page");
+        res.end(html.replace(`{{%CONTENT%}}`,"You are in Home Page"));
+     }else if(path.toLocaleLowerCase()==='/about'){
+        res.end(html.replace(`{{%CONTENT%}}`,"You are in About Page"));
+     }else if(path.toLocaleLowerCase()==='/contact'){
+        res.end(html.replace(`{{%CONTENT%}}`,"You are in Contact Page"));
+     }else if(path.toLocaleLowerCase()==='/products'){
+        res.end(html.replace(`{{%CONTENT%}}`,"You are in Product Page"));
      }else{
-        res.end("Error 404: Page Not Found")
+        res.end(html.replace(`{{%CONTENT%}}`,'Error 404: Page Not Found'))
      }
 })
 server.listen(3000,'127.0.0.1',()=>{
