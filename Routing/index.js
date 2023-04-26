ca70316793292a27d6b857a9d2a353205e1d969c
raobaba <raorajan9576@gmail.com>
2023-04-26 13:25:55 +0530

    setting header in node

--- Routing/index.js
+++ Routing/index.js
@@ -4,14 +4,34 @@ const html = fs.readFileSync('./index.html','utf-8')
 const server = http.createServer((req,res)=>{
      let path = req.url;
      if(path==='/'||path==='/home'){
+        res.writeHead(200,{
+            'Content-Type':'text/html',
+            'My-Header':'Hello,World'
+        })
         res.end(html.replace(`{{%CONTENT%}}`,"You are in Home Page"));
      }else if(path.toLocaleLowerCase()==='/about'){
+        res.writeHead(200,{
+            'Content-Type':'text/html',
+            'My-Header':'Hello,World'
+        })
         res.end(html.replace(`{{%CONTENT%}}`,"You are in About Page"));
      }else if(path.toLocaleLowerCase()==='/contact'){
+        res.writeHead(200,{
+            'Content-Type':'text/html',
+            'My-Header':'Hello,World'
+        })
         res.end(html.replace(`{{%CONTENT%}}`,"You are in Contact Page"));
      }else if(path.toLocaleLowerCase()==='/products'){
+        res.writeHead(200,{
+            'Content-Type':'text/html',
+            'My-Header':'Hello,World'
+        })
         res.end(html.replace(`{{%CONTENT%}}`,"You are in Product Page"));
      }else{
+        res.writeHead(404,{
+            'Content-Type':'text/html',
+            'My-Header':'There is No Route'
+        })
         res.end(html.replace(`{{%CONTENT%}}`,'Error 404: Page Not Found'))
      }
 })
