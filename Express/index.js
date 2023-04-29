7dd91d7fcf8a82125406a975b9a44a712594342e
raobaba <raorajan9576@gmail.com>
2023-04-29 13:30:21 +0530

    GET Request

--- Express/index.js
+++ Express/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
-// app.get('/',(req,res)=>{
-//     res.status(200).send('<h1>Hello, World</h1>')
-// })
-app.get('/',(req,res)=>{
-    res.status(200).json({message:"Hello World",status:200});
+const fs = require('fs');
+const movies = JSON.parse(fs.readFileSync('./Movies.json'));
+
+app.get('/api/v1/movies',(req,res)=>{
+    res.status(200).json({
+        status:"success",
+        count:movies.length,
+        data:{
+            movies:movies
+        }
+    })
 })
+
 app.listen(3000,()=>{
     console.log('Server has Started...');
 })
