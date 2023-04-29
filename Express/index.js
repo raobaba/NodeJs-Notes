4fcf8948064ff636b4a3f53262841a1dc0513bf9
raobaba <raorajan9576@gmail.com>
2023-04-29 14:57:25 +0530

    Handling Delete Request

--- Express/index.js
+++ Express/index.js
@@ -3,8 +3,7 @@ const app = express();
 app.use(express.json());
 const fs = require('fs');
 const movies = JSON.parse(fs.readFileSync('./Movies.json'));
-//movies GET api/v1/movies
-app.get('/api/v1/movies',(req,res)=>{
+const getMovies = (req,res)=>{
     res.status(200).json({
         status:"success",
         count:movies.length,
@@ -12,25 +11,24 @@ app.get('/api/v1/movies',(req,res)=>{
             movies:movies
         }
     })
-})
-//movies GET by ID /api/v1/movies/:id
-app.get('/api/v1/movies/:id',(req,res)=>{
-   const id = req.params.id*1;
-   const movie = movies.find(el=>el.id===id);
-   if(!movie){
-    return res.status(404).json({
-        status:'failed',
-        message:`Movie with the ID ${id} is not Found`
-    })
-   }
-   res.status(200).json({
-    status:'success',
-    data:{
-        movie:movie
+}
+const getMoviesById = (req,res)=>{
+    const id = req.params.id*1;
+    const movie = movies.find(el=>el.id===id);
+    if(!movie){
+     return res.status(404).json({
+         status:'failed',
+         message:`Movie with the ID ${id} is not Found`
+     })
     }
-   })
-})
-app.post('/api/v1/movies',(req,res)=>{
+    res.status(200).json({
+     status:'success',
+     data:{
+         movie:movie
+     }
+    })
+ }
+ const postMovies = (req,res)=>{
     const newId = movies[movies.length-1].id+1;
     const newMovie = Object.assign({id:newId},req.body);
     movies.push(newMovie);
@@ -42,9 +40,8 @@ app.post('/api/v1/movies',(req,res)=>{
             }
         })
     })
-})
-
-app.patch('/api/v1/movies/:id',(req,res)=>{
+}
+const patchMovies = (req,res)=>{
     const id = req.params.id*1;
     const movieToUpdate = movies.find(el=>el.id===id);
     if(!movieToUpdate){
@@ -64,7 +61,42 @@ app.patch('/api/v1/movies/:id',(req,res)=>{
             }
         })
     })
-})
+}
+const deleteMovies = (req,res)=>{
+    const id = req.params.id*1;
+    const movieToDelete = movies.find(el=>el.id===id);
+    if(!movieToDelete){
+        return res.status(404).json({
+            status:'failed',
+            message:`Movie with the ID ${id} is not Found`
+        })
+       }
+    const index = movies.indexOf(movieToDelete);
+    movies.splice(index,1);
+    fs.writeFile('./Movies.json',JSON.stringify(movies),(err)=>{
+        res.status(204).json({
+            status:'success',
+            data:{
+                movie:`Item with ID ${id} is successfully deleted`
+            }
+        })
+    })
+}
+// app.get('/api/v1/movies',getMovies)
+// app.get('/api/v1/movies/:id',getMoviesById)
+// app.post('/api/v1/movies',postMovies)
+// app.patch('/api/v1/movies/:id',patchMovies)
+// app.delete('/api/v1/movies/:id',deleteMovies)
+
+
+app.route('/api/v1/movies')
+.get(getMovies)
+.post(postMovies);
+
+app.route('/api/v1/movies/:id')
+.get(getMoviesById)
+.patch(patchMovies)
+.delete(deleteMovies);
 
 app.listen(3000,()=>{
     console.log('Server has Started...');
