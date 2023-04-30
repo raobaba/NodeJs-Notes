70101412da695468839ded7532af1e1c36c2f651
raobaba <raorajan9576@gmail.com>
2023-04-30 11:00:02 +0530

    serving static file

--- Express/index.js
+++ Express/index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(express.static('./Public'));
 app.use('/api/v1/movies',Router)
 app.listen(3000,()=>{
     console.log('Server has Started...');
