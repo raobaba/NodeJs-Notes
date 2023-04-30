2dbfc71299b0248784cbaff357dce5647518e2bd
raobaba <raorajan9576@gmail.com>
2023-04-30 11:54:07 +0530

    Environment variable

--- Express/index.js
+++ Express/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const Router = require('./Routes/moviesRoutes');
 const morgan = require('morgan');
-
+console.log(app.get('env'))
 app.use(express.json());
-app.use(morgan('tiny'));
+if(process.env.NODE_ENV==='development'){
+    app.use(morgan('dev'));
+}
 app.use(express.static('./Public'));
 app.use('/api/v1/movies',Router)
-app.listen(3000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log('Server has Started...');
 })
