a332e1d3220acf91465a21f778998f0a40f05db1
raobaba <raorajan9576@gmail.com>
2023-05-01 14:06:22 +0530

    setting model

--- Mongodb/Model/movieModel.js
+++ Mongodb/Model/movieModel.js
@@ -2,23 +2,60 @@ const mongoose = require('mongoose');
 
 
 const movieSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        require:[true,'name is required field'],
-        unique:true
-        },
-    description:{
-        type:String,
-        require:[true,'description is required field']
+    name: {
+        type: String,
+        required: [true, 'Name is required field!'],
+        unique: true,
+        trim:true
     },
-    duration:{
-        type:Number,
-        require:[true,'duration is required field']
+    description: {
+        type: String,
+        required: [true, 'Description is required field!'],
+        trim: true
     },
-    rating:{
-        type:Number
-    }
+    duration: {
+        type: Number,
+        required: [true, 'Duration is required field!']
+    },
+    ratings: {
+        type: Number
+    },
+    totalRating: {
+        type: Number
+    },
+    releaseYear: {
+        type: Number,
+        required: [true, 'Release year is required field!']
+    },
+    releaseDate: {
+        type: Date
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        select: false
+    },
+    genres: {
+        type: [String],
+        required: [true, 'Genres is required field!'],
+    },
+    directors: {
+        type: [String],
+        required: [true, 'Directors is required field!']
+    },
+    coverImage: {
+        type: String,
+        require: [true, 'Cover image is required field!']
+    },
+    actors: {
+        type: [String],
+        require: [true, 'actors is required field!']
+    },
+    price: {
+        type: Number,
+        require: [true, 'Price is required field!']
+    },
 })
 
-const movieModel = mongoose.model('movie',movieSchema);
-module.exports=movieModel;
+const movieModel = mongoose.model('movie', movieSchema);
+module.exports = movieModel;
